edadc62e78c8c8af7a37f3ca203fb01f04242944
AJ Javadi <amirjavadi25@gmail.com>
2025-11-24 05:15:12 -0600

    wip

---
+++ public/index.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+//require_once '../src/db.php';
+
+
+echo phpinfo();
+
+/**
+ *
+ * User Class
+ */
+//class User
+//{
+//
+////    public $name;
+////    public $email;
+////    public $role;
+////
+//    public function __construct($name, $email, $role)
+//    {
+//     $this->$name = $name;
+//     $this->$email = $email;
+//     $this->$role = $role;
+//
+//    }
+//
+//    public function getData(): array
+//    {
+//
+//        return [
+//            'name' => $this->$name,
+//            'email' => $this->$email,
+//            'role' => $this->$role
+//        ];
+//    }
+//
+//    public function setData($name, $email, $role)
+//    {
+//        $this->$name = $name;
+//        $this->$email = $email;
+//        $this->$role = $role;
+//    }
+//
+//    public function generateUsername(): string
+//    {
+//        $random_number = random_int(1, 9999);
+//        // takes in name, email, and generates username
+//        return $this->name . $this->email  . $random_number;
+//
+//    }
+//
+//
+//}
+
+//$my_user = new User('John Doe', 'john@example.com', 'admin');
+
+//var_dump($my_user);
+
